43c05ad22a9ce0eede1e0ec45fe7cac2ca1c419d
Lars Nysom <LANY@ucn.dk>
2022-09-13 19:08:47 +0200

    Add project files.

---
+++ InvestmentManager/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Investment Manager 3000");
+Console.WriteLine();
+
+// TODO: Instantiate House and Boat objects 
+
+
+// TODO: Add the objects to an ArrayList
+
+
+// TODO: Iterate through the objects and run either Boat or House methods
+// HINT: Use is operator and Type Casting
+
+int price = 0;
+string? invSummary = null;
+
